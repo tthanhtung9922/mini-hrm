47b0e8289fadc6eaf49783ba74cceaaf642ccdec
tthanhtung9922 <tthanhtung92.work@gmail.com>
2026-03-02 21:43:13 +0700

    feat: add Department EF config and Position config placeholder

---
+++ src/MiniHRM/MiniHRM.Infrastructure/Configurations/PositionConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniHRM.Infrastructure.Configurations
+{
+    internal class PositionConfiguration
+    {
+    }
+}
